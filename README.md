657bf38dda2e31591764d64c80d9ed30d59cabff
alfjuniorbh <alfjuniorbh.web@gmail.com>
2018-02-06 11:01:21 -0400

    Readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# Sistema Financeiro
+
+Micro Sistema Financeiro desenvolvido em PHP
+
+### Instalação
+
+Para instalar o aplicativo, basta serguir os passo abaixo
+
+Requistos
+
+```
+PHP 7x
+Composer
+MySQL
+```
+
+Após o download em um diretório na máquina local, acessar o projeto e seguir os passo abaixo.
+
+```
+cd /financas
+copiar o arquivo .env.example e alterar as credenciais do banco de dados
+composer install
+php -S 127.0.0.1:8000 -t public public/index.php
+```
+
+## Author
+
+* **Junior Ferreira**
--- src/controllers/users.php
+++ src/controllers/users.php
@@ -10,75 +10,106 @@ use Psr\Http\Message\ServerRequestInterface;
 $app
     ->get(
         '/users', function () use ($app) {
-            $view = $app->service('view.renderer');
-            $repository = $app->service('user.repository');
-            $users = $repository->all();
+        $view = $app->service('view.renderer');
+        $repository = $app->service('user.repository');
+        $users = $repository->all();
+
+        if($app->service('auth')->user()->id != 1){
+            return $app->route('users.error');
+        }else{
             return $view->render(
                 'users/list.html.twig', [
-                'users' => $users
+                    'users' => $users
                 ]
             );
-        }, 'users.list'
+        }
+    }, 'users.list'
     )
     ->get(
         '/users/new', function () use ($app) {
-            $view = $app->service('view.renderer');
+        $view = $app->service('view.renderer');
+        if($app->service('auth')->user()->id != 1){
+            return $app->route('users.error');
+        }else{
             return $view->render('users/create.html.twig');
-        }, 'users.new'
+        }
+
+    }, 'users.new'
     )
     ->post(
         '/users/store', function (ServerRequestInterface $request) use ($app) {
-            $data = $request->getParsedBody();
-            $repository = $app->service('user.repository');
-            $auth = $app->service('auth');
-            $data['password'] = $auth->hashPassword($data['password']);
-            $repository->create($data);
-            return $app->route('users.list');
-        }, 'users.store'
+        $data = $request->getParsedBody();
+        $repository = $app->service('user.repository');
+        $auth = $app->service('auth');
+        $data['password'] = $auth->hashPassword($data['password']);
+        $repository->create($data);
+        return $app->route('users.list');
+    }, 'users.store'
     )
     ->get(
         '/users/{id}/edit', function (ServerRequestInterface $request) use ($app) {
-            $view = $app->service('view.renderer');
-            $repository = $app->service('user.repository');
-            $id = $request->getAttribute('id');
-            $user = $repository->find($id);
+        $view = $app->service('view.renderer');
+        $repository = $app->service('user.repository');
+        $id = $request->getAttribute('id');
+        $user = $repository->find($id);
+
+        if($app->service('auth')->user()->id != 1){
+            return $app->route('users.error');
+        }else{
             return $view->render(
                 'users/edit.html.twig', [
-                'user' => $user
+                    'user' => $user
                 ]
             );
-        }, 'users.edit'
+        }
+    }, 'users.edit'
     )
     ->post(
         '/users/{id}/update', function (ServerRequestInterface $request) use ($app) {
-            $repository = $app->service('user.repository');
-            $id = $request->getAttribute('id');
-            $data = $request->getParsedBody();
-            if (isset($data['password'])) {
-                unset($data['password']);
-            }
-            $repository->update($id, $data);
-            return $app->route('users.list');
-        }, 'users.update'
+        $repository = $app->service('user.repository');
+        $id = $request->getAttribute('id');
+        $data = $request->getParsedBody();
+        if (isset($data['password'])) {
+            unset($data['password']);
+        }
+        $repository->update($id, $data);
+        return $app->route('users.list');
+    }, 'users.update'
     )
     ->get(
         '/users/{id}/show', function (ServerRequestInterface $request) use ($app) {
-            $view = $app->service('view.renderer');
-            $repository = $app->service('user.repository');
-            $id = $request->getAttribute('id');
-            $user = $repository->find($id);
+        $view = $app->service('view.renderer');
+        $repository = $app->service('user.repository');
+        $id = $request->getAttribute('id');
+        $user = $repository->find($id);
+
+        if($app->service('auth')->user()->id != 1){
+            return $app->route('users.error');
+        }else{
             return $view->render(
                 'users/show.html.twig', [
-                'user' => $user
+                    'user' => $user
                 ]
             );
-        }, 'users.show'
+        }
+    }, 'users.show'
     )
     ->get(
         '/users/{id}/delete', function (ServerRequestInterface $request) use ($app) {
-            $repository = $app->service('user.repository');
-            $id = $request->getAttribute('id');
+        $repository = $app->service('user.repository');
+        $id = $request->getAttribute('id');
+        if($id == 1){
+            return $app->route('users.error');
+        }else{
             $repository->delete($id);
             return $app->route('users.list');
-        }, 'users.delete'
+        }
+    }, 'users.delete'
+    ) ->get(
+        '/users/error', function () use ($app) {
+        $view = $app->service('view.renderer');
+        $repository = $app->service('user.repository');
+        $users = $repository->all();
+        return $view->render('users/error.html.twig');
+    }, 'users.error'
     );
